6b753403743091c8a0b7789299e68bd336293eb2
Dean <dean@dpb.pw>
2023-03-27 23:15:36 -0700

    Updates to mutations and queries

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -2,19 +2,18 @@
 import { gql } from '@apollo/client';
 
 // Setup for User data and any saved book data loads in 'SavedBooks.js'
-export const GET_ME = gql`
+export const QUERY_ME = gql`
   {
     me {
       _id
       username
       email
-      bookCount
       savedBooks {
         bookId
         authors
+        image
         description
         title
-        image
         link
       }
     }
